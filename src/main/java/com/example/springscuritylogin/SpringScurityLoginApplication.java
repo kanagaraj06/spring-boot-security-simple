c5a32cdf0f903ab678d7fb342342f8b6594aff58
kanagaraj06 <kanagarajmca06@gmail.com>
2020-11-05 21:25:40 +0530

    Simple code

---
+++ src/main/java/com/example/springscuritylogin/SpringScurityLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.springscuritylogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScurityLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringScurityLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springscuritylogin/SpringScurityLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springscuritylogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScurityLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
